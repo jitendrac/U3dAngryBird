89a79b76d7bb29301f0dfd9cecdddd64dd72ffef
lfd <lfd_wy@163.com>
2013-11-11 19:11:57 +0800

    init repository

---
+++ Assets/Scripts/Beginning/Beginning.cs
@@ -0,0 +1,19 @@
+﻿namespace Com.Lfd.Agb.Common{
+	using UnityEngine;
+	using System.Collections;
+	
+	public class Beginning : MonoBehaviour {
+		
+		public Texture2D tt_bglog_logo;
+		
+		// Use this for initialization
+		void Start () {
+			
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+		}
+	}
+}
