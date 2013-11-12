f1a90fc782ca1bf52364042c8db04bb408426733
lfd <lfd_wy@163.com>
2013-11-12 20:35:28 +0800

    I should learn how to use texture atlas,I think

--- Assets/Scripts/Beginning/Beginning.cs
+++ Assets/Scripts/Beginning/Beginning.cs
@@ -1,17 +1,63 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using Com.Lfd.Agb.Common;
 public class Beginning : MonoBehaviour {
 	
-	public Texture2D tt_bglog_logo;
+	private Texture2D tt_splash_01 = null;//first splash image
+	private Texture2D tt_splash_02 = null;//secound splash image
+	private float time_wait_splashping_01 = 2.0f;//the least wait time of secound splash image 
+	private float time_wait_splashping_02 = 5.0f;//the least wait time of secound splash image 
+	private bool isResLoadAllOK = false;//to determine whether the resources loading are complete
+	private bool isShowMainMenu = false;//to determine whether the main menu can be displayed;
 	
-	// Use this for initialization
-	void Start () {
+	//position
+	private Rect rectSplash01;//splash image position
+	private Rect rectSplash02 = new Rect(0,0,Screen.width,Screen.height);//splash02 image position
+	
+	void Start(){
+		StartCoroutine(loadSplash());//load splash image resources
+		StartCoroutine(loadMainMenuResources());//load main menu scence resources
+	}
+	
+	void Update(){
+		if(isShowMainMenu){
+			Application.LoadLevel(1);//load scence main menu scence
+		}
+	}
+	
+	void OnGUI(){
+
+		if(tt_splash_01!=null){
+			rectSplash01 = CommonUtils.RectScreenMiddlePosition(tt_splash_01.width,tt_splash_01.height);
+			GUI.DrawTexture(rectSplash01,tt_splash_01);//draw splash 01 texture
+		}
+		if(tt_splash_02!=null&&Time.time>time_wait_splashping_01){
+			tt_splash_01 = null;
+			CommonUtils.ResTTSplash01 = null;
+			GUI.DrawTexture(rectSplash02,tt_splash_02);//draw splash 02 texture
+			if(isResLoadAllOK&&Time.time>time_wait_splashping_02+time_wait_splashping_01){
+				isShowMainMenu = true;
+				tt_splash_02 = null;
+				CommonUtils.ResTTSplash02 = null;
+				
+			}
+		}		
+	}
+	
+	
+	//load splash resources;
+	IEnumerator loadSplash(){
+		tt_splash_01 = CommonUtils.ResTTSplash01;
+		yield return tt_splash_01;
+	
+		tt_splash_02 = CommonUtils.ResTTSplash02;
+		yield return tt_splash_02;
+	}
+	
+	//load game resources;
+	IEnumerator loadMainMenuResources(){
+		yield return 0;
+		isResLoadAllOK = true;
+	}
 		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
--- Assets/Scripts/Common/CommonUtils.cs
+++ Assets/Scripts/Common/CommonUtils.cs
@@ -2,9 +2,71 @@
 	using UnityEngine;
 	using System.Collections;
 	
+	//delegate
+	public delegate void CallBackMethod0();
+	
 	public class CommonUtils : MonoBehaviour {
 	
+		private static Texture2D _res_tt_splash01 = null;
+		private static Texture2D _res_tt_splash02 = null;
+		private static Texture2D _res_tt_buttons01 = null;
 		
+		public static Texture2D ResTTSplash01{
+			get{
+				if(_res_tt_splash01!=null){
+					return _res_tt_splash01;
+				}
+				Texture2D t2d = Resources.Load("pngs/SPLASHES_SHEET_2",typeof(Texture2D)) as Texture2D;
+				_res_tt_splash01 = t2d;
+				return t2d;
+			}
+			set{
+				_res_tt_splash01 = value;
+			}
+		}
+		
+		//splash 01 texture
+		public static Texture2D ResTTSplash02{
+			get{
+				if(_res_tt_splash02!=null){
+					return _res_tt_splash02;
+				}
+				Texture2D t2d = Resources.Load("pngs/SPLASHES_SHEET_1",typeof(Texture2D)) as Texture2D;
+				_res_tt_splash02 = t2d;
+				return t2d;
+			}
+			set{
+				_res_tt_splash02 = value;
+			}
+		}
+		
+		//wait time and callback a method
+		public static IEnumerator waitTime(float waitTime,CallBackMethod0 callback){
+			yield return new WaitForSeconds(waitTime);
+			callback();
+		}
+		
+		//calculate object position of middle of the screen
+		public static Rect RectScreenMiddlePosition(float width,float height){
+			return new Rect((Screen.width-width)/2.0f,(Screen.height-height)/2.0f,width,height);
+		}
+		
+		//calculate object position of middle of the screen
+		public static Rect RectScreenMiddlePosition(int width,int height){
+			return new Rect((Screen.width-width)/2,(Screen.height-height)/2,width,height);
+		}
+		
+		//load buttons texture
+		private void loadResTTButtons01(){
+				if(_res_tt_buttons01 == null){
+					Texture2D t2d = Resources.Load("pngs/BUTTONS_SHEET_1",typeof(Texture2D)) as Texture2D;	
+				    _res_tt_buttons01 = t2d;
+				}
+		}
+				
+		public static Texture2D GetSubTexture(int x,int y,int height,int width,Texture2D texture){
+			return null;
+		}
 		
 	}
 }
