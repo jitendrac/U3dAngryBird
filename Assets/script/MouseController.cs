65eece0016413ed6cb2ab634f0d1c629085f0465
lfd <lfd_wy@163.com>
2013-11-19 22:26:27 +0800

    by sprite editor,animator

---
+++ Assets/script/MouseController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseController : MonoBehaviour {
+
+	public int firstGameLevelIndex;
+	public GameObject CursorHand;
+	public Sprite SPR_CursorClickMenu;
+	public Sprite SPR_CursorNormalMenu;
+	public Sprite SPR_CursorClickInGame;
+	public Sprite SPR_CursorNormalInGame;
+
+	SpriteRenderer sr_cursorhand;
+	int levelIndex = 0;
+	// Use this for initialization
+	void Start () {
+		Screen.showCursor = false;
+		levelIndex = Application.loadedLevel;
+		sr_cursorhand = CursorHand.GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 v3Mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		CursorHand.transform.position = new Vector3(v3Mouse.x,v3Mouse.y,-9);
+
+		if(levelIndex<firstGameLevelIndex){
+
+			if(Input.GetMouseButton(0)){
+				sr_cursorhand.sprite = SPR_CursorClickMenu;
+			}else{
+				sr_cursorhand.sprite = SPR_CursorNormalMenu;	
+			}
+		}else{
+			if(Input.GetMouseButton(0)){
+				sr_cursorhand.sprite = SPR_CursorClickInGame;
+			}else{
+				sr_cursorhand.sprite = SPR_CursorNormalInGame;	
+			}
+		}
+
+	}
+
+
+}
---
+++ Assets/script/ScaleButtonController.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScaleButtonController : MonoBehaviour {
+
+	public float ScaleValue = 0.1f;
+	public float RotateAngle = 200.0f;
+	public GameObject BarButton;
+
+	private Transform subTransform;
+	private Vector3 scaleOri,scale2big;
+	private int clickCount = 0;
+	private float anglenow = 0f;
+
+	private Animator barButtonAnimator;
+
+	void Start(){
+		scaleOri = transform.localScale;
+		scale2big = new Vector3(scaleOri.x+ScaleValue,scaleOri.y+ScaleValue,scaleOri.z);
+		Transform[] tfs = GetComponentsInChildren<Transform>();
+		foreach(Transform tf in tfs){
+			string goname = tf.gameObject.name;
+			if(goname == "ButtonConfigSub" || goname == "ButtonShareSub"){
+				subTransform = tf;
+				barButtonAnimator = BarButton.GetComponent<Animator>();
+			}
+		}
+	}
+
+	void Update(){
+		if(subTransform != null){
+			if(anglenow > 0 && clickCount == 0){
+				float deltara = -Time.deltaTime*RotateAngle;
+				anglenow += deltara;
+				if(anglenow < 0){
+					anglenow = 0;
+					subTransform.eulerAngles = new Vector3(0,0,0);
+				}else{
+					subTransform.Rotate(Vector3.back*deltara);
+				}
+			}
+			if(anglenow < 180 && clickCount == 1){
+				float deltara = Time.deltaTime*RotateAngle;
+				anglenow += deltara;
+				if(anglenow > 180){
+					anglenow = 180;
+					subTransform.eulerAngles = new Vector3(0,0,180);
+				}else{
+					subTransform.Rotate(Vector3.back*deltara);
+				}
+			}
+		}
+	}
+
+	void OnMouseOver(){
+		gameObject.transform.localScale = scale2big;
+	}
+	
+	void OnMouseExit(){
+		gameObject.transform.localScale = scaleOri;
+	}
+	
+	void OnMouseDown(){
+		clickCount = (++clickCount)==2?0:clickCount;
+		if(clickCount == 1 && subTransform != null){
+			barButtonAnimator.SetBool("IsPlay",true);
+			barButtonAnimator.SetBool("IsHidden",false);
+		}
+		if(clickCount == 0 && subTransform != null){
+			barButtonAnimator.SetBool("IsHidden",true);
+			barButtonAnimator.SetBool("IsPlay",false);
+		}
+	}
+
+	void OnMouseUp(){
+
+	}
+}
